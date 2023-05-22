d5dd589c1916d789a1ff913b6e72f2078cb8b9d1
Max Kaiser <max@kaiser.family>
2023-05-22 15:57:47 +0200

    added styling

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+
+  def new
+    @letters = ('a'..'z').to_a.sample(10).join('')
+  end
+
+  def score
+    @input = params[:input]
+    @grid = params[:letters]
+    word_check_url = "https://wagon-dictionary.herokuapp.com/#{@input}"
+    @word_check = JSON.parse(URI.open(word_check_url).read)
+    @word_found = @word_check["found"]
+
+    word_chars = @input.downcase.chars
+    grid_chars = @grid.chars
+    @include = word_chars.all? { |char| grid_chars.include?(char) && grid_chars.delete(char) }
+
+    @score = @input.length
+  end
+end
